c1db079d83f94eaf75c50a2f226ec40439c9f7de
Muhammad Imam Santosa <imam.santosa@mekari.com>
2026-04-09 13:49:01 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/project
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package agentbugfix
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func plus(a, b int) int {
+	return a + b
+}
+
+func minus(a, b int) int {
+	return a + b
+}
